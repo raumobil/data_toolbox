0b9e576545331f9cd76e55891e6f248bb8def220
Tanja Klopper <t.klopper@raumobil.com>
2022-12-12 11:27:32 +0100

    edited setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='raumo_data_handler',
+setup(name='raumo_data_tools',
       # version='0.1',
       # url='http://github.com/storborg/funniest',
       # author='Flying Circus',
       # author_email='flyingcircus@example.com',
       # license='MIT',
-      packages=['raumo_data_handler'],
+      # packages=['raumo_data_handler'],
       # zip_safe=False
 )
